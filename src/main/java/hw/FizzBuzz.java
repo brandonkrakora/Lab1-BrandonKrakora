febc5fec99196258e47618a50bfc86b85e677385
Brandon <bkrakora@luc.edu>
2018-06-06 15:55:08 -0500

    lab1-part4

--- src/main/java/hw/FizzBuzz.java
+++ src/main/java/hw/FizzBuzz.java
@@ -1,19 +1,64 @@
 package hw;
+import java.util.*;
 
 public class FizzBuzz {
-    public static void fizzbuzz (int n) {
+//    public static void fizzbuzz (int n) {
+//        int i = 1;
+//        while (i <= n) {
+//            if (((i % 3) == 0) && ((i%5) == 0)) {
+//                System.out.println("fizz buzz");
+//            } else if ((i % 5) == 0) {
+//                System.out.println("buzz");
+//            } else if ((i%3) == 0) {
+//                System.out.println("fizz ");
+//            } else {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//    }
+
+    public static String[] fizzbuzz(int n) {
+        String[] fizzBuzzArray;
         int i = 1;
+        if (n <= -1){
+            fizzBuzzArray = new String[1];
+            fizzBuzzArray[0] = "Please enter a posive integer";
+        }
+        else{
+            fizzBuzzArray = new String[n];
+        }
         while (i <= n) {
             if (((i % 3) == 0) && ((i%5) == 0)) {
-                System.out.println("fizz buzz");
+                fizzBuzzArray[i-1] = "fizzbuzz";
             } else if ((i % 5) == 0) {
-                System.out.println("buzz");
+                fizzBuzzArray[i-1] = "buzz";
             } else if ((i%3) == 0) {
-                System.out.println("fizz ");
+                fizzBuzzArray[i-1] = "fizz";
             } else {
-                System.out.println(i);
+                fizzBuzzArray[i-1] = Integer.toString(i);
             }
             i++;
         }
+        return fizzBuzzArray;
+    }
+
+    public static List<String> fizzbuzzlist(int n) {
+        List<String> fizzBuzzList = new ArrayList<>();
+        int i = 1;
+
+        while (i <= n) {
+            if (((i % 3) == 0) && ((i%5) == 0)) {
+                fizzBuzzList.add("fizz buzz");
+            } else if ((i % 5) == 0) {
+                fizzBuzzList.add("buzz");
+            } else if ((i%3) == 0) {
+                fizzBuzzList.add("fizz");
+            } else {
+                fizzBuzzList.add(Integer.toString(i));
+            }
+            i++;
+        }
+        return fizzBuzzList;
     }
 }
--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,18 @@
 package hw;
+import java.util.Scanner;
 
 import java.util.Arrays;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
-  }
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        // FizzBuzz.invokeFizzBuzz(Integer.parseInt(args[0]));
+        String[] returnFizzBuzzArray = fb.fizzbuzz(Integer.parseInt(args[0]));
+        for (int i = 0; i < returnFizzBuzzArray.length; i++) {
+            System.out.println(returnFizzBuzzArray[i]);
+        }
+    }
 }
+
+
